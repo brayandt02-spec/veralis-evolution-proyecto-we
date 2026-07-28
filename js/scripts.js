de4e6cf326a8e421f2686c6e77a05dcdaa728038
brayan <brayandt02@gmail.com>
2026-07-28 02:03:59 -0500

    se modifico el js, cambios importantes en el nav pantallas pequeñasy el css

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-console.log ("veralis evo iniciado")
+
+const menubtn = document.getElementById("menubtn");
+const navlinks = document.getElementById("nav-links");
+const links = navlinks.querySelectorAll("a");
+
+menubtn.addEventListener("click", ()=>{
+    navlinks.classList.toggle("active");
+    if(navlinks.classList.contains("active")){
+         menubtn.innerHTML= "X";
+         menubtn.setAttribute("aria-expanded","true")
+    }else {
+        menubtn.innerHTML ="☰";
+        menubtn.setAttribute("aria-expanded","false")
+    }
+});
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navlinks.classList.remove("active");
+        menubtn.innerHTML = "☰";
+        menubtn.setAttribute("aria-expanded", "false");
+    });
+});
